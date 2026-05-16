49646d6a6cf50946da473375bcf65cad955c8259
K Shah <45090281+ITSecGuru@users.noreply.github.com>
2026-05-16 12:41:29 +1000

    Create stepNames.js

---
+++ src/data/stepNames.js
@@ -0,0 +1,7 @@
+export const stepNames = {
+  "prarthana": { devanagari: "प्रार्थना", roman: "Prarthana", english: "Prayer" },
+  "yogic-jogging-position-1": { devanagari: "योगिक जॉगिंग स्थिति 1", roman: "Yogic Jogging Sthiti 1", english: "Yogic Jogging Position 1" },
+  "yogic-jogging-position-2": { devanagari: "योगिक जॉगिंग स्थिति 2", roman: "Yogic Jogging Sthiti 2", english: "Yogic Jogging Position 2" },
+  "hasta-sanchalan-kriya": { devanagari: "हस्त संचालन क्रिया", roman: "Hasta Sanchalan Kriya", english: "Hand Movement Practice" },
+  "surya-namaskar": { devanagari: "सूर्य नमस्कार", roman: "Surya Namaskar", english: "Sun Salutation" }
+};
